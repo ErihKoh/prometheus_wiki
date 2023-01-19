0c0ca252bc55cabfce5edbc0c0dbc0e9e495203e
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2023-01-19 17:16:26 +0200

    add dinamic title

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,17 +4,24 @@ import markdown
 
 from . import util
 
+    # title = request.path_info.split('/')[-1]
+
 
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
     
 
-def get_article(request, name_article):
+def get_article(request, name_article):    
     res = util.get_entry(name_article)
-    html = markdown.markdown(res)
-    return HttpResponse(html)    
+    markup = markdown.markdown(res)
+    title = request.path_info.split('/')[-1]
+    return render(request, "encyclopedia/article.html", {
+        "title": title,
+        "markup": markup,
+    })    
 
 
