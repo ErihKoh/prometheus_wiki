c9a99fde2659a7652f37761a05b183bece00c5fd
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2023-01-19 16:33:21 +0200

    add foo for get markdown

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:name_article>", views.get_article, name='get_article')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import markdown
 
 from . import util
 
@@ -7,3 +9,12 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+    
+
+def get_article(request, name_article):
+    res = util.get_entry(name_article)
+    html = markdown.markdown(res)
+    return HttpResponse(html)    
+
+
